4c4112a76bdea1a82b353bcb933dd1ee59e16eb0
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-04-22 23:57:44 +0800

    Done creating the Pseudocode

---
+++ odd-even-separator.py
@@ -0,0 +1,13 @@
+#Francis Niño A. Eugenio
+#BSCPE 1-5 | Object Oriented Programing
+#Even & Odd Separator
+
+#Write a Python program that reads a text file named numbers.txt that contains 20 integers. The program will create two other 
+#text file; the first text file will be named even.txt that will contains all even numbers extracted from the numbers.txt. 
+#The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
+
+# read input file
+# for each line
+# separate the even from the list
+# else, store the odd from the list
+# write it in the txt file
