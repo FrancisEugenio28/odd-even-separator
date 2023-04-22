56fc3e42e40db56d19a9b073f47f34011a4308c7
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-04-23 00:11:58 +0800

    In this line, we are trying to separate the even numbers first and put it in empty lists. Unfortunately we exprience some typoError. must fix.

--- odd-even-separator.py
+++ odd-even-separator.py
@@ -6,10 +6,15 @@
 #text file; the first text file will be named even.txt that will contains all even numbers extracted from the numbers.txt. 
 #The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
 
+# create empty list for even and odd
+even = []
+odd = []
 # read input file
 with open("numbers.txt", "r") as input_file:
     # for each line
     for i in (input_file):
-# separate the even from the list
+        # separate the even from the list
+        if (i % 2 == 0):
+            even.append(i)
 # else, store the odd from the list
 # write it in the txt file
